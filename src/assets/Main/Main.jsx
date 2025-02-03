4f23937967d341bade6ad50bcc32f952b706ac8d
Carlos Juatan <carlosjuatan.workplace@gmail.com>
2025-02-03 17:12:08 -0300

    main quase finalizado

--- src/assets/Main/Main.jsx
+++ src/assets/Main/Main.jsx
@@ -7,25 +7,32 @@ function Leftbar({ data }) {
 
     return (
         <main>
+            <div className="destacks_model"></div>
             <div className="destacks_header">
-                <button><h2>all</h2></button>
+                <button id="btn1"><h2>All</h2></button>
                 <button><h2>Music</h2></button>
                 <button><h2>Podcasts</h2></button>
             </div>
             <div className="destacks_container">
                 <div className="destacks_content">
-
+                    {/* ############### Render Recently played ####################*/}
+                    {destacks?.length > 0 && destacks.map((destacks) => (
+                        <button className="card" key={destacks.id}>
+                            <img src={destacks.imgUrl} alt={destacks.name} />
+                            <h4>{destacks.name}</h4>
+                        </button>
+                    ))}
                 </div>
             </div>
-            <div className="albuns_list Recently_played">
+            <div className="Recently_played">
                 <div className="albuns_list_top">
                     <h1>Recently played</h1>
                     <button><h2>Show all</h2></button>
                 </div>
-                <div className="albuns_list_cards">
+                <div className="Recently_played_list_cards">
                     {/* ############### Render Recently played ####################*/}
                     {destacks?.length > 0 && destacks.map((destacks) => (
-                        <button className="card">
+                        <button className="card" key={destacks.id}>
                             <img src={destacks.imgUrl} alt={destacks.name} />
                             <h4>{destacks.name}</h4>
                         </button>
@@ -40,7 +47,7 @@ function Leftbar({ data }) {
                 <div className="albuns_list_cards">
                     {/* ############### Render Made For carlos juatan ####################*/}
                     {destacks?.length > 0 && destacks.map((destacks) => (
-                        <button className="card">
+                        <button className="card" key={destacks.id}>
                             <img src={destacks.imgUrl} alt={destacks.name} />
                             <h4>{destacks.name}</h4>
                         </button>
@@ -55,7 +62,7 @@ function Leftbar({ data }) {
                 <div className="albuns_list_cards">
                     {/* ############### Render Your favorite artists ####################*/}
                     {destacks?.length > 0 && destacks.map((destacks) => (
-                        <button className="card">
+                        <button className="card" key={destacks.id}>
                             <img src={destacks.imgUrl} alt={destacks.name} />
                             <h4>{destacks.name}</h4>
                         </button>
